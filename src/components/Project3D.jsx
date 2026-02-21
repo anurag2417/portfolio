271fe2c2d49a8f07c67c085f8944689a3ed5a1e8
Anurag Kumar <anuragakn18@gmail.com>
2026-02-21 23:33:04 +0530

    feat: implement Project3D component with animated 3D visuals

---
+++ src/components/Project3D.jsx
@@ -0,0 +1,86 @@
+import React, { useRef, useEffect } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Box, Torus, Text, Html } from '@react-three/drei';
+import * as THREE from 'three';
+
+export default function Project3D({ project }) {
+  const groupRef = useRef();
+  const cubeRef = useRef();
+  const ringRef = useRef();
+
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime();
+    
+    if (groupRef.current) {
+      groupRef.current.rotation.y = time * 0.2;
+    }
+    
+    if (cubeRef.current) {
+      cubeRef.current.rotation.x = time * 0.3;
+      cubeRef.current.rotation.y = time * 0.5;
+    }
+    
+    if (ringRef.current) {
+      ringRef.current.rotation.x = time * 0.1;
+      ringRef.current.rotation.z = time * 0.2;
+    }
+  });
+
+  return (
+    <group ref={groupRef} position={[0, 0, 0]}>
+      {/* Main Cube */}
+      <Box ref={cubeRef} args={[1.5, 1.5, 1.5]}>
+        <meshStandardMaterial
+          color={project.color}
+          emissive={project.color}
+          emissiveIntensity={0.5}
+          roughness={0.2}
+          metalness={0.8}
+          transparent
+          opacity={0.9}
+        />
+      </Box>
+
+      {/* Wireframe Cube */}
+      <Box args={[1.8, 1.8, 1.8]}>
+        <meshStandardMaterial
+          color="white"
+          wireframe
+          transparent
+          opacity={0.2}
+        />
+      </Box>
+
+      {/* Orbiting Rings */}
+      <Torus
+        ref={ringRef}
+        args={[1.2, 0.05, 16, 100]}
+        rotation={[Math.PI / 2, 0, 0]}
+      >
+        <meshStandardMaterial color={project.color} emissive={project.color} emissiveIntensity={0.3} />
+      </Torus>
+
+      <Torus
+        args={[1.4, 0.03, 16, 100]}
+        rotation={[0, Math.PI / 2, 0]}
+      >
+        <meshStandardMaterial color="white" emissive="white" emissiveIntensity={0.2} />
+      </Torus>
+
+      {/* Floating Particles */}
+      {Array.from({ length: 20 }).map((_, i) => {
+        const angle = (i / 20) * Math.PI * 2;
+        const radius = 2;
+        const x = Math.cos(angle) * radius;
+        const y = Math.sin(angle) * radius;
+        
+        return (
+          <mesh key={i} position={[x, y, 0]}>
+            <sphereGeometry args={[0.05, 8, 8]} />
+            <meshStandardMaterial color="white" emissive="white" emissiveIntensity={0.5} />
+          </mesh>
+        );
+      })}
+    </group>
+  );
+}
